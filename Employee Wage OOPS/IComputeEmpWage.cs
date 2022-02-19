9da5c279491a38c48f49d9e8cbdd655760362005
Shashikumar <shashirgowdas67@gmail.com>
2022-02-19 17:59:11 +0530

    Added:Code to calculate employee wage using OOPS

---
+++ Employee Wage OOPS/IComputeEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWage_Using_Ooops
+{
+    interface IComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+        void computeEmpWage();
+
+        int getTotalWage(string company);
+    }
+}
---
+++ Employee Wage OOPS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmpWage_Using_Ooops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
